ebfe039d3c27cb4e73097bcb05c3721d16b645cb
Incand <armin-scha@hotmail.de>
2017-08-17 00:53:29 +0200

    Fixed ycm configuration

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-echo "Getting vundle..."
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Installing pacaur-deps..."
+for dep in $(cat pacaur-deps.txt); do
+	pacaur --needed -S "$dep";
+done
+
+if [[ ! -d ~/.vim/bundle ]]; then
+    echo "Getting vundle..."
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 echo "Copying vimrc..."
 cp .vimrc ~/
 
 echo "Installing vim plugins..."
 vim +PluginInstall +qall
+
+echo "Preparing ycm build..."
+cd ~
+mkdir ycm_build
+cd ycm_build
+
+cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+
+echo "Building ycm..."
+cmake --build . --target ycm_core
